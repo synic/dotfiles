59bd63827b364fbd419c08a5e1d2b82c43867821
Adam Olsen <arolsen@gmail.com>
2017-10-16 19:49:08 -0600

    add a few packages to install script

--- scripts/install_ubuntu.py
+++ scripts/install_ubuntu.py
@@ -6,36 +6,44 @@ import urllib
 PACKAGES = """
 python-pip
 python-dev
-vim-gtk3
+vim-gnome
 emacs
 docker
+rofi
 feh
+mosh
+maim
+arandr
 exuberant-ctags
 git
 rxvt-unicode-256color
+thunar
 xclip
 imagemagick
+curl
 xutils-dev
 i3lock
 zsh
 i3
+htop
 """
 
 FILES = (
-    ("https://raw.github.com/synic/urxvt-tabbedex/master/tabbedex",
-     "/usr/lib/urxvt/perl/tabbedex"),
+    ('https://raw.github.com/synic/urxvt-tabbedex/master/tabbedex',
+     '/usr/lib/urxvt/perl/tabbedex'),
 )
 
-os.system("apt update")
-os.system("apt install %s" % PACKAGES.replace("\n", " "))
+os.system('apt update')
+os.system('apt install {}'.format(PACKAGES.replace("\n", " ")))
 
 for remote, local in FILES:
     data = urllib.urlopen(remote).read()
-    h = open(local, "w")
+    h = open(local, 'w')
     h.write(data)
     h.close()
-    print "Wrote %s" % local
+    print 'Wrote {}'.format(local)
 
-os.system("pip install virtualenv virtualenvwrapper mtg")
-os.system("chsh -s /usr/bin/zsh synic")
-os.system("lndir ~/.dotfiles ~")
+os.system('pip install virtualenv virtualenvwrapper mtg')
+os.system('chsh -s /usr/bin/zsh synic')
+os.system('lndir ~/.dotfiles ~')
+os.system('xrdb -merge ~/.Xresources')
