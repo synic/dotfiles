b278d8ae4920f698804fe95c0c68fd570b633af4
Adam Olsen <arolsen@gmail.com>
2017-11-06 12:28:28 -0700

    reorganizing, moving things around, autodock

--- scripts/install_ubuntu.py
+++ scripts/install_ubuntu.py
@@ -1,24 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import urllib
 
 PACKAGES = """
 arandr
+bower
 curl
 docker
 dunst
 emacs
 exuberant-ctags
+gimp
 git
 htop
 i3
 i3lock
 imagemagick
+inkscape
 feh
 maim
+mplayer
 mosh
 net-tools
+nodejs-legacy
+npm
 pasystray
 rofi
 rxvt-unicode-256color
@@ -32,9 +38,13 @@ xclip
 zsh
 """
 
-FILES = (
+REMOTE_FILES = (
     ('https://raw.github.com/synic/urxvt-tabbedex/master/tabbedex',
      '/usr/lib/urxvt/perl/tabbedex'),
+)
+
+REPOS = (
+    ('https://github.com/syl20bnr/spacemacs.git', '~/.emacs.d'),
 )
 
 os.system('apt update')
@@ -43,15 +53,19 @@ os.system('apt install -y {}'.format(PACKAGES.replace('\n', ' ')))
 # remove notify-osd so dunst can take over
 os.system('apt remove notify-osd -y')
 
-for remote, local in FILES:
+for remote, local in REMOTE_FILES:
     data = urllib.urlopen(remote).read()
     h = open(local, 'w')
     h.write(data)
     h.close()
-    print 'Wrote {}'.format(local)
+    print('Wrote {}'.format(local))
+
+for remote, local in REPOS:
+    os.system('rm -rf {}'.format(local))
+    os.system('git clone {} {}'.format(remote, local))
 
 os.system('pip install virtualenv virtualenvwrapper mtg')
 os.system('chsh -s /usr/bin/zsh synic')
-os.system('lndir ~/.dotfiles ~')
+os.system('lndir ~/.dotfiles/dotfiles ~')
 os.system('xrdb -merge ~/.Xresources')
 os.system('chown -R synic:synic /home/synic')
--- zsh.d/01-keybindings.zsh
+++ zsh.d/01-keybindings.zsh
@@ -3,19 +3,7 @@
 bindkey -e
 
 case $TERM in
-    rxvt*|xterm*)
-        bindkey "^[[7~" beginning-of-line #Home key
-        bindkey "^[[8~" end-of-line #End key
-        bindkey "^[[3~" delete-char #Del key
-        bindkey "^[[A" history-beginning-search-backward #Up Arrow
-        bindkey "^[[B" history-beginning-search-forward #Down Arrow
-        bindkey "^[Oc" forward-word # control + right arrow
-        bindkey "^[Od" backward-word # control + left arrow
-        bindkey "^H" backward-kill-word # control + backspace
-        bindkey "^[[3^" kill-word # control + delete
-    ;;
-
-    linux)
+    linux|rxvt*|xterm*)
         bindkey "^[[1~" beginning-of-line #Home key
         bindkey "^[[4~" end-of-line #End key
         bindkey "^[[3~" delete-char #Del key
@@ -23,7 +11,7 @@ case $TERM in
         bindkey "^[[B" history-beginning-search-forward
     ;;
 
-    screen|screen-*)
+    screen|screen-*|tmux)
         bindkey "^[[1~" beginning-of-line #Home key
         bindkey "^[[4~" end-of-line #End key
         bindkey "^[[3~" delete-char #Del key
