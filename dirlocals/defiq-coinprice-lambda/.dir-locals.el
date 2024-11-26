102c340ead94a26671e02b632142b1a82ec0cc32
Adam Olsen <arolsen@gmail.com>
2024-11-26 09:07:12 -0700

    remove defiq-coinprice-lambda dirlocal

--- dirlocals/defiq-coinprice-lambda/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-((python-mode . (
-  (pyvenv-activate . "/home/synic/.virtualenvs/defiq-coinprice-lambda")
-  (flycheck-flake8rc . "/home/synic/Projects/defiq-coinprice-lambda/.flake8")
-  (lsp-pylsp-plugins-flake8-config . "/home/synic/Projects/defiq-coinprice-lambda/.flake8rc")
-  (flycheck-python-mypy-executable . "/home/synic/.virtualenvs/defiq-coinprice-lambda/bin/mypy")
-  (flycheck--automatically-enabled-checkers . (python-flake8))
-  (flycheck--automatically-disabled-checkers . (python-pylint))
-)))
